c6be639fb7da100a8a8f4a2c71388cb1115f095d
AylaDursun <dursun.a@northeastern.edu>
2022-11-30 00:58:30 -0500

    add mobile version

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    var children = this.children;
+    var plus = children.item(1);
+    var minus = children.item(2);
+    if (plus.style.display === "none") {
+      plus.style.display = "block";
+      minus.style.display = "none";
+    } else {
+      plus.style.display = "none";
+      minus.style.display = "block"
+    }
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
